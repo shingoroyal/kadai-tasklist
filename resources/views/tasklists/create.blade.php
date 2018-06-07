ddf2f1047569c4896856e5fe4e49d95f02285733
shingo.sawada <ec2-user@ip-172-31-15-163.us-east-2.compute.internal>
2018-06-07 06:37:18 +0000

    except grid

--- resources/views/tasklists/create.blade.php
+++ resources/views/tasklists/create.blade.php
@@ -5,12 +5,16 @@
     <h1>タスク新規作成ページ</h1>
 
     {!! Form::model($tasklist, ['route' => 'tasklists.store']) !!}
+    <div class="form-group">
         {!! Form::label('status', 'ステータス:') !!}
-        {!! Form::text('status') !!}
+        {!! Form::text('status',null,['class' =>'form-controll']) !!}
+    </div>
+    <div class="form-group">
         {!! Form::label('content', 'タスク:') !!}
-        {!! Form::text('content') !!}
+        {!! Form::text('content',null,['class' =>'form-controll']) !!}
+    </div>
 
-        {!! Form::submit('投稿') !!}
+        {!! Form::submit('投稿',['class' => 'btn btn-primary']) !!}
 
     {!! Form::close() !!}
 
--- resources/views/tasklists/edit.blade.php
+++ resources/views/tasklists/edit.blade.php
@@ -4,13 +4,18 @@
 
     <h1>id: {{ $tasklist->id }} のタスク編集ページ</h1>
 
-    {!! Form::model($tasklist, ['route' => ['tasklists.update', $tasklist->id], 'method' => 'put']) !!}
+    
+    {!! Form::model($tasklist, ['route' => ['tasklists.update',$tasklist->id],'method'=>'put']) !!}
+    <div class="form-group">
         {!! Form::label('status', 'ステータス:') !!}
-        {!! Form::text('status') !!}
+        {!! Form::text('status',null,['class' =>'form-controll']) !!}
+    </div>
+    <div class="form-group">
         {!! Form::label('content', 'タスク:') !!}
-        {!! Form::text('content') !!}
+        {!! Form::text('content',null,['class' =>'form-controll']) !!}
+    </div>
 
-        {!! Form::submit('更新') !!}
+        {!! Form::submit('投稿',['class' => 'btn btn-default']) !!}
 
     {!! Form::close() !!}
 
