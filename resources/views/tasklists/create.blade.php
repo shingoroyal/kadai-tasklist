c23fd735e447fe7681cc6dfa07409816a30023b2
shingo.sawada <ec2-user@ip-172-31-15-163.us-east-2.compute.internal>
2018-06-08 00:44:12 +0000

    modify the grid

--- resources/views/tasklists/create.blade.php
+++ resources/views/tasklists/create.blade.php
@@ -4,25 +4,22 @@
 
     <h1>タスク新規作成ページ</h1>
 <div class="row">
-        <div class="col-xs-12">
-        <div class="col-sm-offset-2 col-sm-8">
-        <div class="col-lg-offset-3 col-lg-6">
+        <div class="col-xs-12　col-sm-offset-2 col-sm-8　col-lg-offset-3 col-lg-6">
     {!! Form::model($tasklist, ['route' => 'tasklists.store']) !!}
     <div class="form-group">
         {!! Form::label('status', 'ステータス:') !!}
-        {!! Form::text('status',null,['class' =>'form-controll']) !!}
+        {!! Form::text('status',null,['class' =>'form-control']) !!}
     </div>
     <div class="form-group">
         {!! Form::label('content', 'タスク:') !!}
-        {!! Form::text('content',null,['class' =>'form-controll']) !!}
+        {!! Form::text('content',null,['class' =>'form-control']) !!}
     </div>
 
         {!! Form::submit('投稿',['class' => 'btn btn-primary']) !!}
 
     {!! Form::close() !!}
     </div>
-    </div>
-    </div>
+    
  </div>
 
 @endsection
