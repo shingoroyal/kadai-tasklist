e49e0b21c6421d918d4e753f96df86c516cdb6e4
shingo.sawada <ec2-user@ip-172-31-15-163.us-east-2.compute.internal>
2018-06-19 01:31:37 +0000

    modify create again

--- resources/views/tasklists/create.blade.php
+++ resources/views/tasklists/create.blade.php
@@ -6,7 +6,7 @@
 
     {!! Form::model($tasklist, ['route' => 'tasklists.store']) !!}
      <div class="form-group">
-         {!! Form::label('status, 'ステータス') !!}
+         {!! Form::label('status', 'ステータス') !!}
         {!! Form::text('status', null, ['class' => 'form-control']) !!}
      </div>
      <div class="form-group">
